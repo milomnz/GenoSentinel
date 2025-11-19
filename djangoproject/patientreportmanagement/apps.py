e3875aff900e30f890132dd84b30705b347e029c
camy <mariacamymendez@gmail.com>
2025-11-19 17:10:12 -0500

    Proyecto base

---
+++ djangoproject/patientreportmanagement/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PatientreportmanagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'patientreportmanagement'
